058e8ffbd0ddb6abd429a7fe2ae1b1aa28e3f455
Neeli Mani Vignesh <Neeli Mani Vignesh@mneeli-Z1099889H>
2023-08-04 23:22:09 +0530

    Initial Commit

---
+++ src/test/java/com/projecte/ProjectEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
